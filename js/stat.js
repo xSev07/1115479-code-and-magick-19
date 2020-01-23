8223f6eda0bd6ae93993a2dcb31c496bb5f729ed
Artemiy <artemiy.dmitrochenko@gmail.com>
2020-01-23 20:19:07 +0500

    Paint cloud and text

--- js/stat.js
+++ js/stat.js
@@ -1,6 +1,21 @@
 'use strict';
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
+var CLOUD_X = 100;
+var CLOUD_Y = 10;
+var GAP = 10;
+
+var renderCloud = function(ctx, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+}
 
 window.renderStatistics = function(ctx, names, times) {
-  ctx.fillStyle = "#444";
-  ctx.fillRect(100, 10, 420, 270);
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, 'white');
+
+  ctx.fillStyle = 'black';
+  ctx.font = '16px PT Mono';
+  ctx.fillText('Ура вы победили!',CLOUD_X + GAP * 3, CLOUD_Y + GAP * 3);
+  ctx.fillText('Список результатов:',CLOUD_X + GAP * 3, CLOUD_Y + GAP * 5);
 }
