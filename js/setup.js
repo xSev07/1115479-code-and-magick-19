a08bfc38524b905fa25d8b12dff4589b22f1e0e2
Artemiy <Artemiy.Dmitrochenko@gmail.com>
2020-01-28 15:18:33 +0500

    Add setup.js and create data massives

---
+++ js/setup.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var userDialog = document.querySelector('.setup');
+userDialog.classList.remove('hidden');
+
+var firstNames = [
+  'Иван',
+  'Хуан Себастьян',
+  'Мария',
+  'Кристоф',
+  'Виктор',
+  'Юлия',
+  'Люпита',
+  'Вашингтон'
+];
+var secondNames = [
+  'да Марья',
+  'Верон',
+  'Мирабелла',
+  'Вальц',
+  'Онопко',
+  'Топольницкая',
+  'Нионго',
+  'Ирвинг'
+];
+var coatColors = [
+  'rgb (101, 137, 164)',
+  'rgb (241, 43, 107)',
+  'rgb (146, 100, 161)',
+  'rgb (56, 159, 117)',
+  'rgb (215, 210, 55)',
+  'rgb (0, 0, 0)'
+];
+var eyesColors = [
+  'black',
+  'red',
+  'blue',
+  'yellow',
+  'green'
+];
